677a2d520cba6f6b4d2fcb8c34488017ff9b82dd
Patrick Naughton <patrikjnaughton@gmail.com>
2014-09-25 22:58:14 -0700

    added in fraction files...

---
+++ Euler/Fraction.swift
@@ -0,0 +1,135 @@
+//
+//  Fraction.swift
+//  Euler
+//
+//  Created by Patrick Naughton on 6/10/14.
+//  Copyright (c) 2014 Orange Banana. All rights reserved.
+//
+
+import Foundation
+
+public class Fraction {
+    var n: Int
+    var d: Int
+
+    public init(n:Int, d:Int) {
+        self.n = n
+        self.d = d
+    }
+
+    public func toString() -> String {
+        return "\(n) / \(d)"
+    }
+
+    public func value() -> Double {
+        return Double(self.n) / Double(self.d)
+    }
+
+    public class func random(n: Int) -> Fraction {
+        return Fraction(n: Int(arc4random()) % n, d: Int(arc4random()) % n);
+    }
+}
+
+public func gcd(var u: Int, var v: Int) -> Int {
+    if u == 0 {return v}
+    if v == 0 {return u}
+    if u < 0 {u = -u}
+    if v < 0 {v = -v}
+    var shift = 0
+    while ((u | v) & 1) == 0 {
+        u >>= 1
+        v >>= 1
+        ++shift
+    }
+    while (u & 1) == 0 {
+        u >>= 1;
+    }
+    do {
+        while (v & 1) == 0 {
+            v >>= 1
+        }
+        if u > v {
+            let t = v
+            v = u
+            u = t
+        }
+        v -= u
+    } while v != 0
+    return u << shift
+}
+
+public func == (left: Fraction, right: Fraction) -> Bool {
+    return left.n == right.n && left.d == right.d
+}
+
+public func != (left: Fraction, right: Fraction) -> Bool {
+    return left.n != right.n || left.d != right.d
+}
+
+public func * (left: Fraction, right: Fraction) -> Fraction {
+    var n = left.n * right.n
+    var d = left.d * right.d
+    let g = gcd(n, d)
+    n /= g
+    d /= g
+    return Fraction(n: n, d: d)
+}
+
+public func * (left: Int, right: Fraction) -> Fraction {
+    return Fraction(n: left, d: 1) * right
+}
+
+public func * (left: Fraction, right: Int) -> Fraction {
+    return left * Fraction(n: right, d: 1)
+}
+
+public func / (left: Fraction, right: Fraction) -> Fraction {
+    var n = left.n * right.d
+    var d = left.d * right.n
+    let g = gcd(n, d)
+    n /= g
+    d /= g
+    return Fraction(n: n, d: d)
+}
+
+public func / (left: Int, right: Fraction) -> Fraction {
+    return Fraction(n: left, d: 1) / right
+}
+
+public func / (left: Fraction, right: Int) -> Fraction {
+    return left / Fraction(n: right, d: 1)
+}
+
+public func + (left: Fraction, right: Fraction) -> Fraction {
+    var n = left.n * right.d + right.n * left.d
+    var d = left.d * right.d
+    let g = gcd(n, d)
+    n /= g
+    d /= g
+    return Fraction(n: n, d: d)
+}
+
+public func + (left: Int, right: Fraction) -> Fraction {
+    return Fraction(n: left, d: 1) + right
+}
+
+public func + (left: Fraction, right: Int) -> Fraction {
+    return left + Fraction(n: right, d: 1)
+}
+
+public func - (left: Fraction, right: Fraction) -> Fraction {
+    var n = left.n * right.d - right.n * left.d
+    var d = left.d * right.d
+    let g = gcd(n, d)
+    n /= g
+    d /= g
+    return Fraction(n: n, d: d)
+}
+
+public func - (left: Int, right: Fraction) -> Fraction {
+    return Fraction(n: left, d: 1) - right
+}
+
+public func - (left: Fraction, right: Int) -> Fraction {
+    return left - Fraction(n: right, d: 1)
+}
---
+++ Euler/FractionNode.swift
@@ -0,0 +1,59 @@
+//
+//  FractionNode.swift
+//  Euler
+//
+//  Created by Patrick Naughton on 6/12/14.
+//  Copyright (c) 2014 Orange Banana. All rights reserved.
+//
+
+import SpriteKit
+
+class FractionNode : SKNode
+{
+    var mFraction: Fraction
+    var nLabel: SKLabelNode
+    var dLabel: SKLabelNode
+
+    let size = 64
+    let gap = 4
+
+    override init() {
+        let f = Fraction.random(100)
+        nLabel = FractionNode.label("numerator", n:f.n, x:0, y:gap)
+        dLabel = FractionNode.label("denominator", n:f.d, x:0, y:-gap)
+        dLabel.position = CGPoint(x:dLabel.position.x, y:dLabel.position.y - dLabel.frame.height)
+        var divideLine = SKShapeNode(rectOfSize: CGSize(width: dLabel.frame.size.width + 40,height: 4))
+        divideLine.position = CGPointMake(0,0);
+        divideLine.strokeColor =  UIColor.orangeColor()
+        divideLine.fillColor = divideLine.strokeColor
+
+        var s = nLabel.frame
+        mFraction = f
+        super.init()
+        name = "fraction"
+        addChild(nLabel)
+        addChild(dLabel)
+        addChild(divideLine)
+     }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func update() {
+        nLabel.text = String(mFraction.n);
+        dLabel.text = String(mFraction.d);
+    }
+
+    class func label(name: String, n: Int, x: Int, y: Int) -> SKLabelNode
+    {
+        let label = SKLabelNode(fontNamed:"Optima-Bold")
+        label.horizontalAlignmentMode =  SKLabelHorizontalAlignmentMode.Center
+        label.name = name
+        label.text = String(n)
+        label.fontSize = 64
+        label.position = CGPoint(x:x, y: y)
+        return label
+    }
+
+}
